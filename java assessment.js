10d7c8c2f5538fe61f2b5d51792614f3bbc1281e
ArYaN14138 <148914861+ArYaN14138@users.noreply.github.com>
2024-05-27 16:29:10 +0530

    Update java assessment.js

--- java assessment.js
+++ java assessment.js
@@ -1,9 +1,7 @@
-// Create a variable to hold your NFTs
+
 const NFTs = [];
 
-// This function will take in some values as parameters, create an
-// NFT object using the parameters passed to it for its metadata, 
-// and store it in the variable above.
+
 function mintNFT(_name, _eyeColor, _shirtType, _bling) {
     const NFT = {
         name: _name,
@@ -15,8 +13,7 @@ function mintNFT(_name, _eyeColor, _shirtType, _bling) {
     console.log("Minted: " + _name);
 }
 
-// Create a "loop" that will go through an "array" of NFTs
-// and print their metadata with console.log()
+
 function listNFTs() {
     for (let i = 0; i < NFTs.length; i++) {
         console.log("\nID: \t\t" + (i + 1));
@@ -27,12 +24,12 @@ function listNFTs() {
     }
 }
 
-// Print the total number of NFTs we have minted to the console
+
 function getTotalSupply() {
     console.log("\nTotal Supply: " + NFTs.length);
 }
 
-// Call your functions below this line
+
 mintNFT("Bob", "Blue", "Hoodie", "Gold Chain");
 mintNFT("Sue", "Green", "T-Shirt", "Silver Bracelet");
 mintNFT("Tim", "Brown", "Jacket", "Platinum Ring");
